8452c0501d00cd5a311297094ec27e28b4e8dc2c
MiGy <42974724+4nn0nym05@users.noreply.github.com>
2022-04-12 22:46:32 +0200

    Add files via upload

---
+++ GetFileHash/Install.ps1
@@ -0,0 +1,16 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+$scriptpath = (Get-ChildItem .\Get-Hash.ps1 ).FullName
+$command = "powershell.exe -WindowStyle Hidden -command &`"$scriptpath`""
+
+Set-Location -LiteralPath Registry::HKEY_CLASSES_ROOT\*\shell
+New-Item .\GetFileHash -Force
+Set-Item .\GetFileHash "Get File Hash"
+New-ItemProperty .\GetFileHash -Name 'Icon' -Value "powershell.exe" -PropertyType String
+New-ItemProperty .\GetFileHash -Name 'Position' -Value "top" -PropertyType String
+New-Item .\GetFileHash\command -Force
+Set-Item .\GetFileHash\command "$command '%V'"
+
